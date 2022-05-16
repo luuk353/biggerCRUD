bf43530bee47a54d778533a4bd56f266c87939bd
Daniel <danielkuhlmann@upcmail.nl>
2022-05-16 15:26:45 +0200

    contactform + index aanpassingen DJ

--- contact.php
+++ contact.php
@@ -32,18 +32,28 @@
                 </li>
             </ul>
         </nav>
-        <h1 id="maintext">placeholder name</h1>
+        <div class="contentblok">
+            <form>
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name..">
 
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name..">
 
-        <article id="maininfo">
-            <h2>ik weet niett</h2>
-            <p>Jug phd speed</p>
-        </article>
+                <label for="country">Country</label>
+                <select id="country" name="country">
+                    <option value="australia">Australia</option>
+                    <option value="canada">Canada</option>
+                    <option value="usa">USA</option>
+                </select>
 
-        <article id="sideinfo">
-            <h3>xdd</h3>
-            <p> xddd </p>
-        </article>
+                <label for="subject">Subject</label>
+                <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+
     </main>
     <?php include("phpincludes/footertemplate.php")?>
 </body>

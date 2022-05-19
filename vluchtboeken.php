658bb18cecd9e8f34b3bddefcc68219259c29b08
Daniel <danielkuhlmann@upcmail.nl>
2022-05-19 11:44:50 +0200

    aanpassingen/vluchtboeken

---
+++ vluchtboeken.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Website</title>
+</head>
+
+<body>
+    <?php include("phpincludes/headertemplate.php")?>
+    <main>
+        <?php include ("phpincludes/navbar.php")?>
+        <div class ="container1">
+            <p>container</p>
+        </div>
+    </main>
+    <?php include("phpincludes/footertemplate.php")?>
+</body>
+
+</html>

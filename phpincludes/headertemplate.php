5f2b8a5715344ccb757cb5f355c30f7d94c26802
Daniel <danielkuhlmann@upcmail.nl>
2022-05-23 15:25:54 +0200

    fixen

--- phpincludes/headertemplate.php
+++ phpincludes/headertemplate.php
@@ -1,5 +1,5 @@
 <div id="header">
-    <div class="bepisfoto">
-        <img src="img/cat+looking+out+of+plane.png" alt="Trulli" width="500" height="333">
+    <div> 
+        <img src="img/cat+looking+out+of+plane.png" alt="Trulli" width="100" height="100">
     </div>
 </div>
